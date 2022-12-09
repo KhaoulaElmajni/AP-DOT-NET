bb863accacfd573b185ecc9b3d6b6f5dedc792aa
Khaoula Elmajni <elmajnikhaoula99@gmail.com>
2022-12-10 00:44:10 +0100

    adding product controller

--- MyWebApi/Program.cs
+++ MyWebApi/Program.cs
@@ -1,19 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using MYWEBAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ProductContext>(opt =>
+    opt.UseInMemoryDatabase("ProductContext"));
+//builder.Services.AddSwaggerGen(c =>
+//{
+//    c.SwaggerDoc("v1", new() { Title = "TodoApi", Version = "v1" });
+//});
+
+/*builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();*/
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
